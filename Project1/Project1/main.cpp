51bee76432e5f6003fbc6456f0d76d2ea29dcc7c
FilipKwas <filikwa678@student.polsl.pl>
2020-10-15 12:59:34 +0200

    Fibonacci iterative

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,33 +1,38 @@
 #include <iostream>
-
-int pow(int base, int power) {
-	int result = 1;
-
-	for (int i = 0; i < power; i++) {
-		result *= base; 
+int fibonacci_iterative(int index) {
+	int a = 0;
+	int b = 1;
+	
+	if (index == 0 || index == 1) {
+		return index;
 	}
 
-	return result;
+	for (int i = 2; i < index; i++) {
+		int tmp = a + b; 
+		a = b;
+		b = tmp;
+	}
+	
+	return b;
 }
-
 
 int read_int() {
 	std::cout << "Please introduce a number: " << std::endl;
 
 	int n;
 	std::cin >> n;
+	
 	return n;
 }
 
 
 int main() {
+	int index = read_int();
+	
 
-	int base = read_int();
-	int power = read_int();
+	int result = fibonacci_iterative(index);
 
-	int result = pow(base, power);
-
-std::cout << "The result is: " << result << std::endl;
+	std::cout << "The result is: " << result << std::endl;
 
 		return 0;
 }
