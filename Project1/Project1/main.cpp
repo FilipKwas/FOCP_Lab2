8dd3bdc5c39b2366c7531bdfdb4f16f24c05e25a
FilipKwas <filikwa678@student.polsl.pl>
2020-10-15 14:05:28 +0200

    Example read/write to file

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,49 +1,41 @@
 #include <iostream>
-int fibonacci_iterative(int index) {
-	int a = 0;
-	int b = 1;
-	
-	if (index < 2) {
-		return index;
-	}
-
-	for (int i = 1; i < index; i++) {
-		int tmp = a + b; 
-		a = b;
-		b = tmp;
-	}
-	
-	return b;
-}
-
-
-int fibonacci_recursive(int index) {
-	if (index < 2) {
-		return index;
-	}
-
-	return fibonacci_recursive(index - 2) + fibonacci_recursive(index - 1);
-
-}
-
-
-int read_int() {
-	std::cout << "Please introduce a number: " << std::endl;
-
-	int n;
-	std::cin >> n;
-	
-	return n;
-}
-
+#include <fstream>
+#include <string>
 
 int main() {
-	int index = read_int();
+
+	//1- Open/Create the file stream
+	//2- While the file is open, you can write
+	//3- Close the file in order to keep the changes
+
+	std::ofstream file;
+	file.open("/Users/Filip/FOCP_Lab2/new_file.txt");
+	file << "This is the first line!\n";
+	file << "This is the secong line!\n";
+	file << "This is the third line!\n";
+	file.close();
+
+	//1- Open/Create the file stream
+	//2- While the file is open, you can read
+	//3- Print the contents of the file
+	//4- Close the file in order to keep the changes
+
+	 std::ifstream new_file("/Users/Filip/FOCP_Lab2/new_file.txt");
+
+	if (new_file.is_open()) {
 	
+		std::string line;
 
-	int result = fibonacci_recursive(index);
+		while(getline(new_file, line)) {
+			std::cout << line << std::endl;
+		}
+	
+		new_file.close();
 
-	std::cout << "The result is: " << result << std::endl;
+	} else {
+		std::cout << "There was a problem opening the file";
+	}
+
 
 		return 0;
 }
