29546312b4457245c0ccbb3674fe434c20fffac8
FilipKwas <filikwa678@student.polsl.pl>
2020-10-15 12:14:12 +0200

    Find prime number

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,7 +1,24 @@
+#include <iostream>
+
 int main() {
 	
-	
-	
-	return 0;
+	// 1 - Read a number form a console
+	// 2 - Figure out if the number is prime or not
+	// 3 - Print the result to the console
 
+	std::cout << "Please introduce a number: " << std::endl;
+
+	int number;
+	std::cin >> number;
+
+	for (int i = 2; i < number; i++) {
+		if (number % i == 0) {
+			std::cout << "Your number is not prime" << std::endl;
+			return 0;
+		}
+	}
+	
+	std::cout << "Your number is prime" << std::endl;
+
+	return 0;
 }
